519a43a9a4f9ee073ab64df101224afd045edcdc
94angu <94angath@gmail.com>
2018-06-10 22:40:36 +0530

    login part added

---
+++ vGuide/app/src/main/java/com/example/hp/vguide/Login.java
@@ -0,0 +1,196 @@
+package com.example.hp.vguide;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
+
+    EditText Email, Password;
+    Button LogInButton, RegisterButton;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+    FirebaseUser mUser;
+    String email, password;
+    ProgressDialog dialog;
+    public static final String userEmail="";
+
+    public static final String TAG="LOGIN";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        LogInButton = (Button) findViewById(R.id.buttonLogin);
+
+        RegisterButton = (Button) findViewById(R.id.buttonRegister);
+
+        Email = (EditText) findViewById(R.id.editEmail);
+        Password = (EditText) findViewById(R.id.editPassword);
+        dialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (mUser != null) {
+                    if(isServicesOK()){
+                        Intent intent = new Intent(Login.this, MapActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                }
+                else
+                {
+                    Log.d(TAG,"AuthStateChanged:Logout");
+                }
+
+            }
+        };
+        // LogInButton.setOnClickListener((View.OnClickListener) this);
+        //RegisterButton.setOnClickListener((View.OnClickListener) this);
+        //Adding click listener to log in button.
+        LogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Calling EditText is empty or no method.
+                userSign();
+
+
+            }
+        });
+
+        // Adding click listener to register button.
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Opening new user registration activity using intent on button click.
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(Login.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(Login.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //removeAuthSateListner is used  in onStart function just for checking purposes,it helps in logging you out.
+        mAuth.removeAuthStateListener(mAuthListner);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListner != null) {
+            mAuth.removeAuthStateListener(mAuthListner);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Login.super.finish();
+    }
+
+
+
+    private void userSign() {
+        email = Email.getText().toString().trim();
+        password = Password.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(Login.this, "Enter the correct Email", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(Login.this, "Enter the correct password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        dialog.setMessage("Loging in please wait...");
+        dialog.setIndeterminate(true);
+        dialog.show();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    dialog.dismiss();
+
+                    Toast.makeText(Login.this, "Login not successfull", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    dialog.dismiss();
+
+                    checkIfEmailVerified();
+
+                }
+            }
+        });
+
+    }
+    //This function helps in verifying whether the email is verified or not.
+    private void checkIfEmailVerified(){
+        FirebaseUser users=FirebaseAuth.getInstance().getCurrentUser();
+        boolean emailVerified=users.isEmailVerified();
+        if(!emailVerified){
+            Toast.makeText(this,"Verify the Email Id",Toast.LENGTH_SHORT).show();
+            mAuth.signOut();
+            finish();
+        }
+        else {
+            Email.getText().clear();
+
+            Password.getText().clear();
+            Intent intent = new Intent(Login.this, MapActivity.class);
+
+            // Sending Email to Dashboard Activity using intent.
+            intent.putExtra(userEmail,email);
+
+            startActivity(intent);
+
+        }
+    }
+}
