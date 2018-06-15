eba4b43bf28f6e6cdf322d97b9df5b1e18d8da9e
94angu <94angath@gmail.com>
2018-06-15 15:30:04 +0530

    Google API key changed

--- vGuide/app/src/main/java/com/example/hp/vguide/models/DirectionFinder.java
+++ vGuide/app/src/main/java/com/example/hp/vguide/models/DirectionFinder.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class DirectionFinder {
     private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?";
 //    private static final String GOOGLE_API_KEY = "AIzaSyDnwLF2-WfK8cVZt9OoDYJ9Y8kspXhEHfI";
-    private static final String GOOGLE_API_KEY = "AIzaSyCPYDdPlpEfz6Xq4BGLJF6ITjGb1X2NdgI";
+    private static final String GOOGLE_API_KEY = "AIzaSyCEVXAUK65iVQU3ltxISkPaOJcGxPjJ3pk";
     private DirectionFinderListener listener;
     private String origin;
     private String destination;
